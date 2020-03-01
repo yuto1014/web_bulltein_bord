831c65f6c298f3e23384757506a708bf60dc1112
yuto1014 <yto.oct.14@gmail.com>
2020-03-01 09:43:12 +0000

    [Update]投稿一覧画面更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_084828) do
+ActiveRecord::Schema.define(version: 2020_03_01_092116) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -43,9 +43,10 @@ ActiveRecord::Schema.define(version: 2020_02_29_084828) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
+    t.string "name", default: "no name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
