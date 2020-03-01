8f54598c153604a087d202a0325896de9ced7d19
yuto1014 <yto.oct.14@gmail.com>
2020-03-01 16:58:18 +0000

    [Update]コメント機能更新

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -1,2 +1,25 @@
 class Users::CommentsController < ApplicationController
+
+	def create
+      @post = Post.find(params[:post_id])
+      #投稿に紐づいたコメントを作成
+      @comment = @post.comments.build(comment_params)
+      @comment.user_id = current_user.id
+      if @comment.save
+      	render :index
+      end
+    end
+
+    def destroy
+      @comment = Comment.find(params[:id])
+      if @comment.destroy
+      	render :index
+      end
+    end
+
+    private
+    def comment_params
+      params.require(:comment).permit(:content, :post_id, :user_id, :reply)
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   	has_many :categories, through: :category_posts
 	belongs_to :user
 	has_many :comments, dependent: :destroy
+	has_many :replies, class_name: "Comment", foreign_key: :reply, dependent: :destroy
 	#refile
 	attachment :image
 
